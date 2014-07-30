d12e33aa56845499b90e0b267d58fb0f27c32d26
MaiP <mai.phan@eastagile.com>
2014-07-30 10:48:56 +0700

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by EA on 7/30/14.
+//  Copyright (c) 2014 Mai Phan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
